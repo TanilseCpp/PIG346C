c108be1fa7b44a4e1d4a0794632409c493b8d7db
Tanilse <tanilsec@gmail.com>
2026-01-23 15:42:48 -0500

    POO al 50%

--- Mision1/Proyecto1/src/com/empresa/gestion/model/model/Gerente.java
+++ Mision1/Proyecto1/src/com/empresa/gestion/model/model/Gerente.java
@@ -2,10 +2,13 @@ package com.empresa.gestion.model.model;
 
 public class Gerente extends Empleado{
     private String departamento;
+    private double bono;
 
-    public Gerente(String nombre, int edad, double salario, String departamento){
-        super(nombre, edad, salario);
+
+    public Gerente(String nombre, int edad, boolean activo, char genero, double salario, int horasTrabajadas, double bono, String departamento){
+        super(nombre, edad, activo, genero, salario, horasTrabajadas);
         this.departamento = departamento;
+        this.bono = bono;
     }
 
     public String getDepartamento(){
@@ -16,9 +19,23 @@ public class Gerente extends Empleado{
         this.departamento = departamento;
     }
 
+    public double getBono() {
+        return bono;
+    }
+
+    public void setBono(double bono) {
+        this.bono = bono;
+    }
+
     //♻️Sobreescritura de metodo, Polimorfismo
     @Override
     public void presentarse(){
         System.out.println("Soy el gerente "+getNombre()+" y estoy a cargo del departamento de "+departamento);
+    }
+
+    @Override
+    public double calcularSalarioMensual() {
+        double salario = super.calcularSalarioMensual();
+        return salario + bono;
     }
 }
